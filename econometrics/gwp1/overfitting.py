4db46ba3fd0e2b2f2002ebde072ae5828d08100a
Pankaj Kumar <pankaj.tiwari2@gmail.com>
2022-11-26 19:23:27 +0800

    adding for wqu econometric proj 1

---
+++ econometrics/gwp1/overfitting.py
@@ -0,0 +1,10 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+
+y = np.array([1,   4 , 8 , 17 , 24, 34])
+x= range(1, len(y)+1)
+X = np.array([[a] for a in x])
+regrssion_lin = LinearRegression()
+res_lin = regrssion_lin.fit(X, y)
+
