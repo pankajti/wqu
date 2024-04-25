4b26f2c75085e4d96ee568bf0036c7961c001b66
Pankaj Kumar <pankaj.tiwari2@gmail.com>
2024-04-26 07:23:02 +0800

    capstone module 6 commits

---
+++ capstone/analysis/breakout_strategy_analysis.py
@@ -0,0 +1,17 @@
+import schedule
+import time
+import datetime as dt
+import logging
+import pandas as pd
+import numpy as np
+import pytz
+import sqlite3
+import sqlalchemy
+
+db_data_path = r'/Users/pankajti/dev/git/wqu/capstone/data/db/capstone.db'
+con = sqlite3.connect(db_data_path)
+dbEngine = sqlalchemy.create_engine(f'sqlite:///{db_data_path}')
+
+
+trades = pd.read_sql("select * from trades",dbEngine)
+print(trades.shape)
