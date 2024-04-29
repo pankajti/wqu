52a6972cd7efe09386171cf21a8cd331ce8636a4
Pankaj Kumar <pankaj.tiwari2@gmail.com>
2024-04-30 02:57:03 +0800

    capstone module 6 commits

--- capstone/breakout_trading_system/signals/intraday/open_range_breakout.py
+++ capstone/breakout_trading_system/signals/intraday/open_range_breakout.py
@@ -1,47 +1,59 @@
+import schedule
+import time
+import datetime as dt
 import logging
 import pandas as pd
+import numpy as np
+import pytz
+import time
+import sqlite3
+import sqlalchemy
+from capstone.breakout_trading_system.signals.intraday.price_breakout import get_signals
 
-
+version = int(time.time())
 logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 logger.info("Log initiated ")
 
-def get_signals(market_data):
-    try:
 
-        thresold=5
-        num_of_candles_to_check=5
-        logger.info("started loading signals ")
-        ticker_groups = market_data.groupby('ticker')
-        all_transactions=[]
-        for ticker , candle_data in ticker_groups:
-            close_hist = pd.concat([candle_data.Close.shift(i).rename(f'Close_{i}') for i in range(0, num_of_candles_to_check+1)], axis=1)
-            close_hist_inc = pd.concat(
-                [(close_hist.iloc[:, i] < close_hist.iloc[:, i - 1]).rename(f'Close_{i}_cmp') for i in
-                 range(1, num_of_candles_to_check+1)], axis=1)
-            close_hist_dec = pd.concat(
-                [(close_hist.iloc[:, i] > close_hist.iloc[:, i - 1]).rename(f'Close_{i}_cmp') for i in
-                 range(1, num_of_candles_to_check+1)], axis=1)
-            close_hist['change'] = ((close_hist['Close_0'] - close_hist[f'Close_{num_of_candles_to_check}']))
-            close_hist['change_perc'] = 100 * (close_hist['change'] / close_hist[f'Close_{num_of_candles_to_check}'])
-            buy_breakout = close_hist[(close_hist_inc.sum(axis=1) == num_of_candles_to_check) & (close_hist['change_perc']>thresold)].assign(transactio_type='buy')
-            sell_breakout = close_hist[(close_hist_dec.sum(axis=1) == num_of_candles_to_check)& (close_hist['change_perc']>thresold)].assign(transactio_type='sell')
-            transaction = pd.concat([buy_breakout,sell_breakout])
-            transaction['ticker'] = ticker
-            all_transactions.append(transaction.drop_duplicates(['ticker'], keep='last'))
+db_data_path = r'/Users/pankajti/dev/git/wqu/capstone/data/db/capstone.db'
+con = sqlite3.connect(db_data_path)
+dbEngine = sqlalchemy.create_engine(f'sqlite:///{db_data_path}')
 
-        signals = pd.concat(all_transactions)
-        ret_signals = signals[['ticker', 'transactio_type', 'Close_0']].reset_index()
+class OrbSignal():
+    def __init__(self, market_data):
+        self.market_data_close = market_data.pivot_table(index='timestamp', columns='ticker', values='Close').fillna(
+            method='ffill')
+        self.min_df = self.market_data_close[:35].min().dropna()
+        self.min_df.name='min_val'
+        self.max_df = self.market_data_close[:35].max().dropna()
+        self.max_df.name='max_val'
+
+
+    def get_signals(self, minute_candle):
+        timestamp = minute_candle.index.max()
+        ticker_prices=minute_candle.loc[timestamp][['ticker', 'Close']].set_index('ticker')
+        data = ticker_prices.join(self.min_df).join(self.max_df)
+        data['buy']=data.apply(lambda x : 1 if x['Close']>x['max_val'] else 0, axis =1)
+        data['sell']=data.apply(lambda x : 1 if x['Close']<x['min_val'] else 0, axis =1)
+        data['side'] = data.buy+-1*data.sell
+        data=data[data.side!=0]
+        data['trade_time']=timestamp
+        ret_signals = data.reset_index()[['trade_time', 'ticker', 'side', 'Close']]
         ret_signals.columns = ['trade_time', 'ticker', 'side', 'price']
-    except Exception as e:
-        logger.info("error ")
+        return ret_signals
 
-    return ret_signals
+
+        print(data)
+
+def get_signals(market_data):
+    print(market_data.shape)
 
 def main():
-    md=[]
-    get_signals(market_data=md)
+    md= pd.read_sql("select * from market_data where date = '2024-03-27'", dbEngine)
+    orb_signal= OrbSignal(md)
+    orb_signal.get_signals(minute_candle=md)
 
 if __name__ == '__main__':
     main()
